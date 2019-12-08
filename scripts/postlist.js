4108d6ac438aeedec1cab8f2f8f7e42fda1e1edf
amitborkar98 <amitborkar98@gmail.com>
2019-12-08 12:11:00 +0530

    Added the box shadow on posts on mouseover events

--- scripts/postlist.js
+++ scripts/postlist.js
@@ -16,3 +16,15 @@ var no = document.getElementById("no");
 no.onclick = function() {
     deleteModal.style.display = "none";
   } 
+
+//function to display the box shadow over post on mouseover event
+function boxShadow(id){
+  var post = document.getElementById(id);
+  post.style.boxShadow = "2px 20px lightgray";
+  }
+
+//function to remove box shadow of post on mouseout event
+function removeShadow(id){
+  var post = document.getElementById(id);
+  post.style.boxShadow = "0px 0px";
+  }
