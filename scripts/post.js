17ccbdd36d857281f3ab71bf9ee49346f4bfcaf2
amitborkar98 <amitborkar98@gmail.com>
2019-12-09 11:12:59 +0530

    Implemented Like functionality

--- scripts/post.js
+++ scripts/post.js
@@ -14,16 +14,27 @@ edit.onclick = function(){
         title.style.borderColor="";
         title.style.borderStyle="";
         edit.value=edit.defaultValue;
-
     }
 }
-
 
 function addComments(id){
     var comment = id.value +'<br>';
     var textarea = document.getElementById('userComments');
     document.getElementById('comments').style.padding="10px";
     document.getElementById('comments').innerHTML += '<p class="comment-text">'+comment+'</p>';          
-    textarea.value=textarea.defaultValue;
-    
+    textarea.value=textarea.defaultValue;   
 }
+
+var numberOfClicks=0;
+function postLiked(){
+    document.getElementById("like").innerHTML = "<i class='fa fa-thumbs-up'></i> Liked!";
+    numberOfClicks += 1;
+    if(numberOfClicks!=0){
+        if (numberOfClicks==1){
+            document.getElementById('like-desc').innerHTML = numberOfClicks + " person likes this!";
+        }
+        else {
+            document.getElementById('like-desc').innerHTML = numberOfClicks + " people have liked this!";
+        }
+        }
+    }
